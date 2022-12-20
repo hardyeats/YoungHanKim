a6c0d6b4761bd84a3c76eb13618e7d7a6d6be22c
hardyeats <hardyeats702@gmail.com>
2022-12-20 16:11:36 +0900

    첫 테스트 성공.

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,1 +1,10 @@
+import { IMemberService } from "./member/IMemberService";
+import { Grade, Member } from "./member/Member";
+import { MemberService } from "./member/MemberService";
 
+const memberService: IMemberService = new MemberService();
+const member = new Member(1, "memberA", Grade.VIP);
+memberService.join(member);
+const findedMember = memberService.findMember(1);
+console.log("new member:", member);
+console.log("finded member:", findedMember);

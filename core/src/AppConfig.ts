d15fbc45484a4403efea3399afc82ca322dea4d5
hardyeats <hardyeats702@gmail.com>
2022-12-27 10:55:48 +0900

    Refactor AppConfig

--- core/src/AppConfig.ts
+++ core/src/AppConfig.ts
@@ -1,4 +1,6 @@
+import { DiscountPolicy } from "./discount/DiscountPolicy";
 import { FixedAmountDiscountPolicy } from "./discount/FixedAmountDiscountPolicy";
+import { IMemberRepository } from "./member/IMemberRepository";
 import { IMemberService } from "./member/IMemberService";
 import { MemberService } from "./member/MemberService";
 import { MemoryMemberRepository } from "./member/MemoryMemberRepository";
@@ -7,12 +9,15 @@ import { OrderService } from "./order/OrderService";
 
 export class AppConfig {
   memberService(): IMemberService {
-    return new MemberService(new MemoryMemberRepository());
+    return new MemberService(this.memberRepository());
   }
   orderService(): IOrderService {
-    return new OrderService(
-      new MemoryMemberRepository(),
-      new FixedAmountDiscountPolicy()
-    );
+    return new OrderService(this.memberRepository(), this.discountPolicy());
+  }
+  memberRepository(): IMemberRepository {
+    return new MemoryMemberRepository();
+  }
+  discountPolicy(): DiscountPolicy {
+    return new FixedAmountDiscountPolicy();
   }
 }
